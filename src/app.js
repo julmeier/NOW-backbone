2e4cd3f5e3c0ece37d975fa6955cba7e70c4498d
Julia <julia.meier@gmail.com>
2017-11-27 15:18:47 -0800

    rendered a single instance of the song model in the browser via a template

--- src/app.js
+++ src/app.js
@@ -8,6 +8,46 @@ import './css/style.css';
 
 console.log('it loaded!');
 
+import Song from './models/song';
+
+const bathroomWindow = new Song({
+  title: 'She Came In Through the Bathroom Window',
+  year: 1969,
+  artist: 'The Beatles',
+});
+console.log(bathroomWindow);
+
+const songData = [
+    {
+      title: "Dancing in the Moonlight",
+      year: 1972,
+      artist: "King Harvest"
+    },
+    {
+      title: "Highway to Hell",
+      year: 1979,
+      artist: "AC/DC"
+    },
+    {
+      title: "What Do You Mean",
+      year: 2015,
+      artist: "Justin Bieber"
+    },
+    {
+      title: "Hello Goodbye",
+      year: 1967,
+      artist: "The Beatles"
+    },
+  ];
+
+let songTemplate;
+
 $(document).ready( () => {
-  
+
+  songTemplate = _.template($('#song-template').html());
+
+  $('#song-list').append(songTemplate(bathroomWindow.attributes));
+
+  console.log('in the doc ready section');
+
 });
